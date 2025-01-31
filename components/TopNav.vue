ccbd19b49a3266a2076c87a05a7a0fd6373f9c64
Cartic Natraj <cnatraj@gmail.com>
2025-01-31 15:32:52 -0800

    added topnav

---
+++ components/TopNav.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row class="py-6" no-gutters align="center">
+    <v-col>
+      <v-img :src="logo" :width="150" cover />
+    </v-col>
+    <v-col class="text-right">
+      <v-btn class="elevatedButton" color="secondary">Get Started</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+const logo = new URL("../assets/images/spellyLogo.png", import.meta.url).href;
+</script>
