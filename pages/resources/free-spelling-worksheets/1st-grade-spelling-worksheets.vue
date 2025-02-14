226462d78b66673a5539900b61b852853a89db1b
Cartic Natraj <cnatraj@gmail.com>
2025-02-13 21:23:21 -0800

    base pages

---
+++ pages/resources/free-spelling-worksheets/1st-grade-spelling-worksheets.vue
@@ -0,0 +1,8 @@
+<template>
+  <ContentPage :resource="resource" />
+</template>
+
+<script setup>
+import resource from "@/data/resources/free-spelling-worksheets/1st-grade-spelling-worksheets.json";
+import ContentPage from "../../../components/content/contentPage.vue";
+</script>
---
+++ pages/resources/free-spelling-worksheets/4th-grade-spelling-worksheets.vue
@@ -0,0 +1,8 @@
+<template>
+  <ContentPage :resource="resource" />
+</template>
+
+<script setup>
+import resource from "@/data/resources/free-spelling-worksheets/4th-grade-spelling-worksheets.json";
+import ContentPage from "../../../components/content/contentPage.vue";
+</script>
