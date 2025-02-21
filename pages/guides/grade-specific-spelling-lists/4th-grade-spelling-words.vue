44e2806163849befe8b83b2fde9f02f2956f25b6
Cartic Natraj <cnatraj@gmail.com>
2025-02-21 10:36:43 -0800

    4th grade spelling words

--- pages/guides/grade-specific-spelling-lists/4th-grade-spelling-words.vue
+++ pages/guides/grade-specific-spelling-lists/4th-grade-spelling-words.vue
@@ -1,3 +1,8 @@
-<template>4th Grade Spelling Words</template>
+<template>
+  <ContentPage :resource="resource" />
+</template>
 
-<script setup></script>
+<script setup>
+import ContentPage from "../../../components/content/contentPage.vue";
+import resource from "../../../data/guides/grade-specific-spelling-lists/4th-grade-spelling-words.json";
+</script>
