53f19e4d13916aefdf4474f4ecd316734f6d7dca
Cartic Natraj <cnatraj@gmail.com>
2025-02-22 02:53:58 -0800

    4th and 5th grade content pages

---
+++ pages/resources/spelling-lists/4th-grade-spelling-words.vue
@@ -0,0 +1,9 @@
+<template>
+  <ContentPage :resource="resource" />
+</template>
+
+<script setup>
+  import resource from '@/data/resources/spelling-lists/4th-grade-spelling-words.json'
+  import ContentPage from '../../../components/content/contentPage.vue'
+</script>
+ye
