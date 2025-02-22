c091ebd4c0f157afeeefad362f04986572ef9f29
Cartic Natraj <cnatraj@gmail.com>
2025-02-22 02:15:15 -0800

    3rd grade spelling list

---
+++ pages/resources/spelling-lists/3rd-grade-spelling-words.vue
@@ -0,0 +1,9 @@
+<template>
+  <ContentPage :resource="resource" />
+</template>
+
+<script setup>
+  import resource from '@/data/resources/spelling-lists/3rd-grade-spelling-words.json'
+  import ContentPage from '../../../components/content/contentPage.vue'
+</script>
+ye
