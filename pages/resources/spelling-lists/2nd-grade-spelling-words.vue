5e9ced1d71fac3d23a6749775fff160d9a8a5b08
Cartic Natraj <cnatraj@gmail.com>
2025-02-22 02:01:52 -0800

    2nd grade spelling words

---
+++ pages/resources/spelling-lists/2nd-grade-spelling-words.vue
@@ -0,0 +1,9 @@
+<template>
+  <ContentPage :resource="resource" />
+</template>
+
+<script setup>
+  import resource from '@/data/resources/spelling-lists/2nd-grade-spelling-words.json'
+  import ContentPage from '../../../components/content/contentPage.vue'
+</script>
+ye
