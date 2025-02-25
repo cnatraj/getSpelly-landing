df133f708c0ab75ad54075dd5d11c22ee82003cd
cnatraj <cnatraj@gmail.com>
2025-02-25 08:41:16 -0800

    2 more content pages

---
+++ pages/resources/commonly-misspelled-words/top-50.vue
@@ -0,0 +1,9 @@
+<template>
+  <ContentPage :resource="resource" />
+</template>
+
+<script setup>
+  import resource from '@/data/resources/commonly-misspelled-words/top-50.json'
+
+  import ContentPage from '../../../components/content/contentPage.vue'
+</script>
