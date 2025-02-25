b82287102bcf0b30a724a74348b7c097eccbc3a1
Cartic Natraj <cnatraj@gmail.com>
2025-02-25 11:55:30 -0800

    more content files

---
+++ pages/resources/spelling-bee/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <ContentPage :resource="resource" />
+</template>
+
+<script setup>
+  import resource from '@/data/resources/spelling-bee/index.json'
+
+  import ContentPage from '../../../components/content/contentPage.vue'
+</script>
